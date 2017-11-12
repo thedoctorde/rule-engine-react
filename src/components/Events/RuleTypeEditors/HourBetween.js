187374cf1365bf76f8fa89f61d1aa6e4441b7f84
Dmitry <dmitry@appnext.com>
2017-11-12 11:22:58 +0300

    Fix bug #7378 Change label format for time "00:00 - 23:00"

--- src/components/Events/RuleTypeEditors/HourBetween.js
+++ src/components/Events/RuleTypeEditors/HourBetween.js
@@ -6,7 +6,7 @@ const wrapperStyle = {
   display: "flex",
   flexWrap: "wrap",
 };
-const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+const arr = [{"value":0,"label":"00:00"},{"value":1,"label":"01:00"},{"value":2,"label":"02:00"},{"value":3,"label":"03:00"},{"value":4,"label":"04:00"},{"value":5,"label":"05:00"},{"value":6,"label":"06:00"},{"value":7,"label":"07:00"},{"value":8,"label":"08:00"},{"value":9,"label":"09:00"},{"value":10,"label":"10:00"},{"value":11,"label":"11:00"},{"value":12,"label":"12:00"},{"value":13,"label":"13:00"},{"value":14,"label":"14:00"},{"value":15,"label":"15:00"},{"value":16,"label":"16:00"},{"value":17,"label":"17:00"},{"value":18,"label":"18:00"},{"value":19,"label":"19:00"},{"value":20,"label":"20:00"},{"value":21,"label":"21:00"},{"value":22,"label":"22:00"},{"value":23,"label":"23:00"}];
 const HourBetween = ({valueFrom, valueTo, handleChangeFrom, handleChangeTo}) => {
   return(
     <div style={wrapperStyle}>
@@ -15,8 +15,8 @@ const HourBetween = ({valueFrom, valueTo, handleChangeFrom, handleChangeTo}) => 
         value={valueFrom}
         onChange={handleChangeFrom}>
         {
-          arr.map(hour => {
-          return <MenuItem value={hour} primaryText={hour+""} key={hour+""}/>
+          arr.map(item => {
+          return <MenuItem value={item.value} primaryText={item.label} key={item.value}/>
         }) }
       </SelectField>
       <SelectField
@@ -24,8 +24,8 @@ const HourBetween = ({valueFrom, valueTo, handleChangeFrom, handleChangeTo}) => 
         value={valueTo}
         onChange={handleChangeTo}>
         {
-          arr.map(hour => {
-          return <MenuItem value={hour} primaryText={hour+""} key={hour+""}/>
+          arr.map(item => {
+          return <MenuItem value={item.value} primaryText={item.label} key={item.value}/>
         }) }
       </SelectField>
     </div>
